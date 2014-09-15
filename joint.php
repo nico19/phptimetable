961581d291496ebbf07607a04ec506b65c4c8c5d
Yuriy Pigovsky <pigovsky@gmail.com>
2014-09-15 16:06:54 +0300

    now lessons are formatted in js, but without knockout

--- joint.php
+++ joint.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Cайт Піговського Ю.Р.</title>
-<meta charset="utf-8">
-<link rel="stylesheet" type="text/css" href="styles.css" />
-</head>
-<body> 
-<table>
 <?php 
-$charset = "utf8";
-
 $colors=[
 "Aqua",
 "Aquamarine",
@@ -35,71 +24,11 @@ $colors=[
 "Tomato"
 ];
 
-$teachers = [
-	$_REQUEST["teacher"]
-	/*"Блашків%О.%В",
-	"Гурик%М.%І",
-	"Юрчишин%Т.%В.%",
-	"Якубовський%Р.%В",
-	"Добротвор%І.%Г.%",
-	"Бубняк",
-	"Палій%І.%О.%",
-	"Дорош%В.%І.%",
-	"Лукащук-Федик%С.%В",
-	"Саченко%А.%О.%",
-	"Римар%О.%Л.%",	
-	"Файфура ",	
-	"Коваль%В.%С.",	
-	"Майків ",
-	"Биковий ",
-	"Турченко%І.",
-	"Гладій ",
-	"Ляпандра ",
-	"Пукас ",
-	"Яцків%Н.",
-	"Пасічник%Р.%М.",
-	"Турченко%В.",*/
-];
+$teachers = json_decode($_REQUEST["teacher"]);	
 
-$times = [
-	"08:00:00",
-	"09:35:00",
-	"11:10:00",
-	"12:50:00",
-	"14:25:00",
-	"16:00:00",
-	"17:35:00",
-	"19:00:00",
-	"20:15:00"
-];
 
-$days=[
-"Понеділок",
-"Вівторок",
-"Середа",
-"Четвер",
-"П'ятниця"
-];
 
-$oddEven=[
-"чисельник",
-"знаменник"
-];
-
-$dbname = "timetable";
-$user = "root";
-$pwd="";
-$host ="localhost";
-$con=mysqli_connect($host,$user,$pwd,$dbname);
-
-// Check connection
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  return;
-}
-mysqli_query ($con,"set character_set_client='$charset'"); 
-mysqli_query ($con,"set character_set_results='$charset'"); 
-mysqli_query ($con,"set collation_connection='".$charset."_general_ci'"); 
+include "connect.php";
 
 $lessons=[];
 $color = 0;
@@ -109,44 +38,23 @@ foreach ($teachers as $teacher) {
 	foreach (getTeacherLessons($con, $teacher) as $lesson) {
 		$t = $lesson->teacher;
 		$r = $lesson->room;
-		$lessons[$lesson->day][$lesson->lesson_time][$lesson->week][]="<p style='background-color:$colorString'>$t в а.$r</p>";
+		$txt = "<p style='background-color:$colorString'>$t в а.$r</p>";
+		$day = $lesson->day;
+		$time = $lesson->lesson_time;
+		$week = $lesson->week;
+		if (!(array_key_exists($day, $lessons) &&
+			array_key_exists($time, $lessons[$day]) &&
+				array_key_exists($week, $lessons[$day][$time]) &&
+					in_array($txt, $lessons[$day][$time][$week]))) {
+						$lessons[$day][$time][$week][]=$txt;
+		}
 	}	
 	$color++;
 }
 
-
-for($day=1; $day<=5; $day++) {	
-	$dayString = $days[$day-1];
-	print "<tr><td rowspan=". count($times)*2 ."> $dayString </td>";
-	foreach ($times as $time) {
-		print "<td rowspan=2>$time</td>";
-		foreach([1, 2] as $week){
-			$weekString = $oddEven[$week-1];
-			print "<td>$weekString </td> <td>";
-			
-			if (! (array_key_exists($day, $lessons) &&
-					array_key_exists($time, $lessons[$day]) &&
-						array_key_exists($week, $lessons[$day][$time]) ) ) {
-							print "</td></tr>";
-							continue;							
-			}
-			
-			$ll = $lessons[$day][$time][$week];			
-			if ($ll) {
-				if (is_array($ll)) {
-					foreach(array_unique($ll, SORT_REGULAR) as $lesson) {					
-						print $lesson;
-					}
-				} else {
-					print $ll;
-				}
-			}
-			print "</td></tr>";
-		}
-	}	
-}
-
 mysqli_close($con);
+
+print json_encode($lessons);
 
 function getTeacherLessons($con, $teacher){
 	$sth = mysqli_query($con,"SELECT * from timetable where teacher like '%$teacher%'");	
@@ -160,6 +68,3 @@ function getTeacherLessons($con, $teacher){
 }
 
 ?>
-</table>
-</body>
-</html>
--- js/teacherAutocomplete.js
+++ js/teacherAutocomplete.js
@@ -1,5 +1,62 @@
 var AUTOCOMPLETE_ID = "autocomplete";
 
+var $times = [
+		"08:00:00",
+		"09:35:00",
+		"11:10:00",
+		"12:50:00",
+		"14:25:00",
+		"16:00:00",
+		"17:35:00",
+		"19:00:00",
+		"20:15:00"
+	];
+
+var $days=[
+		"Понеділок",
+		"Вівторок",
+		"Середа",
+		"Четвер",
+		"П'ятниця"
+	];	
+
+var $oddEven=[
+		"чисельник",
+		"знаменник"
+	];
+
+function print($lessons){
+	var html = "";
+	for($day=1; $day<=5; $day++) {	
+		$dayString = $days[$day-1];
+		html += "<tr><td rowspan=" + $times.length*2 +">" + $dayString + "</td>";
+		$times.forEach(function ($time) {
+			html+= "<td rowspan=2>" + $time + "</td>";
+			for($week=1; $week<=2; $week++){
+				$weekString = $oddEven[$week-1];
+				html+= "<td>" + $weekString + "</td> <td>";
+				
+				if (! (($day in $lessons) &&
+						($time in $lessons[$day]) &&
+							($week in $lessons[$day][$time]) ) ) {
+								html+= "</td></tr>";
+								continue;							
+				}
+				
+				$ll = $lessons[$day][$time][$week];			
+				if ($ll) {					
+					$ll.forEach(function($lesson) {					
+						html+= $lesson;
+					});					
+				}
+				html+= "</td></tr>";
+			}
+		});	
+	}
+	document.getElementById("timetable").innerHTML = html;
+}
+
+
 function addAutocomplete(id){
 	$( "#autocomplete" +id ).autocomplete({
 	  source: function(request, response)
@@ -54,9 +111,9 @@ function TeacherLessonsViewModel()
 		for(var i=1; i<=self.teacherCount; i++) {
 			$('#'+AUTOCOMPLETE_ID+i).val(teachers[i-1]);
 		}
-	};
+	};		
+		
 	
-	self.lessons = ko.observableArray();
     self.readLessons = 
 		function(){ 
 			$.ajax({
@@ -66,15 +123,16 @@ function TeacherLessonsViewModel()
 			})
 		.done(function( msg ) {
 			console.log(msg);
-			msg = JSON.parse(msg);	
-			msg = msg.rows;
-			msg.forEach(function(e){console.log(e);});
-			self.lessons(msg);
+			msg = JSON.parse(msg);				
+			//msg.forEach(function(e){console.log(e);});
+			//self.lessons(msg);
+			print(msg)
 		  })
 		.fail(function(msg){ 
-			self.lessons([msg]);
+			print([msg]);
 		} );
 	};
+	
 	self.addTeacher = function(){
 		self.teacherCount++;
 		var id = '"autocomplete'+self.teacherCount+'"';
